5f2e9a1fdce5f89f45d7adc68ae016dc227661ce
WonJunPyun <84018317+WonJunPyun@users.noreply.github.com>
2021-06-21 15:26:38 +0900

    algorism
    algorism

---
+++ 25_1934_baekjun.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def gcd(a, b) :
+    if b==0:
+        return a
+    else :
+        return gcd(b,a%b)
+
+for _ in range(n):
+    a , b = map(int,input().split())
+    g  = gcd(a,b)
+    print(int(g*(a/g)*(b/g)))
